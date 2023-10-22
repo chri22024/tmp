4138df010fca3736bb44072c10f1893908307a3b
rinnnn55 <125744070+rinnnn55@users.noreply.github.com>
2023-10-22 22:37:15 +0900

    e

--- src/Header.jsx
+++ src/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
                 
 
                 <h1>Birdwatching</h1>
-                <img src="/Users/rinichimrt/Desktop/school/web_pgm/react-practice/imags/dove.png" alt="a simple dove logo"></img>
+                <img src="imags/dove.png" alt="a simple dove logo"></img>
     
             
             </header>
